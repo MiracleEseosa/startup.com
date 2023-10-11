a674cd1164469b477fa748c7a27cc669278e407d
MiracleEseosa <110738316+MiracleEseosa@users.noreply.github.com>
2023-10-11 03:04:59 -0700

    Add files via upload

---
+++ start up project/hamb.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var hamburger =
+document.querySelector(".hamburger");
+    var menu =
+document.querySelector(".menu");
+
+hamburger.addEventListener("click", function() {
+    menu.classList.toggle("show");
+});
+})
---
+++ start up project/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var dropdowns =
+document.getElementsByClassName("dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+
+dropdowns[i].addEventListener("mouseover", function() {
+this.getElementsByClassName("dropdown-content")[0].style.display = "block";
+        });
+dropdowns[i].addEventListener("mouseout",function() {
+
+this.getElementsByClassName("dropdown-content")[0].style.display = "none";
+        });
+    }
+});
+
+
+
